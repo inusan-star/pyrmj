03a900ba0db6b83053b8a35041ea8ffe8ae63611
yokoyamaharuki <haruki.yokoyama@rooter.co.jp>
2024-06-26 21:52:04 +0900

    変更

--- src/pyrmj/hoora.py
+++ src/pyrmj/hoora.py
@@ -4,31 +4,31 @@ def get_pre_yaku(yaku):
     """
     pre_yaku = []
 
-    if yaku.get("riichi") == 1:
+    if yaku["riichi"] == 1:
         pre_yaku.append({"name": "立直", "hansuu": 1})
 
-    if yaku.get("riichi") == 2:
+    if yaku["riichi"] == 2:
         pre_yaku.append({"name": "ダブル立直", "hansuu": 2})
 
-    if yaku.get("ippatsu"):
+    if yaku["ippatsu"]:
         pre_yaku.append({"name": "一発", "hansuu": 1})
 
-    if yaku.get("haitei") == 1:
+    if yaku["haitei"] == 1:
         pre_yaku.append({"name": "海底摸月", "hansuu": 1})
 
-    if yaku.get("haitei") == 2:
+    if yaku["haitei"] == 2:
         pre_yaku.append({"name": "河底撈魚", "hansuu": 1})
 
-    if yaku.get("rinshan"):
+    if yaku["rinshan"]:
         pre_yaku.append({"name": "嶺上開花", "hansuu": 1})
 
-    if yaku.get("chankan"):
+    if yaku["chankan"]:
         pre_yaku.append({"name": "槍槓", "hansuu": 1})
 
-    if yaku.get("tenhoo") == 1:
+    if yaku["tenhoo"] == 1:
         pre_yaku = [{"name": "天和", "hansuu": "*"}]
 
-    if yaku.get("tenhoo") == 2:
+    if yaku["tenhoo"] == 2:
         pre_yaku = [{"name": "地和", "hansuu": "*"}]
 
     return pre_yaku
