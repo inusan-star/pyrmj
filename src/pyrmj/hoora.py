a74209bfa000a240b1b1cd22f4c5679ba48b8c77
yokoyamaharuki <haruki.yokoyama@rooter.co.jp>
2024-06-29 22:37:21 +0900

    懸賞役判定追加

--- src/pyrmj/hoora.py
+++ src/pyrmj/hoora.py
@@ -1,3 +1,7 @@
+import re
+from .yama import Yama
+
+
 def get_pre_yaku(yaku):
     """
     状況役一覧を取得する
@@ -32,3 +36,61 @@ def get_pre_yaku(yaku):
         pre_yaku = [{"name": "地和", "hansuu": "*"}]
 
     return pre_yaku
+
+
+def get_post_hupai(tehai, ron_hai, dora, uradora):
+    """
+    懸賞役一覧を取得する
+    """
+    new_tehai = tehai.clone()
+
+    if ron_hai:
+        new_tehai.tsumo(ron_hai)
+
+    tehai_string = new_tehai.to_string()
+    post_yaku = []
+    suit_string_list = re.findall(r"[mpsz][^mpsz,]*", tehai_string)
+    n_dora = 0
+
+    for hai in dora:
+        hai = Yama.dora(hai)
+        regexp = re.compile(hai[1])
+
+        for suit_string in suit_string_list:
+            if suit_string[0] != hai[0]:
+                continue
+
+            suit_string = suit_string.replace("0", "5")
+            numbers = regexp.findall(suit_string)
+
+            if numbers:
+                n_dora += len(numbers)
+
+    if n_dora:
+        post_yaku.append({"name": "ドラ", "hansuu": n_dora})
+
+    n_akahai = tehai_string.count("0")
+
+    if n_akahai > 0:
+        post_yaku.append({"name": "赤ドラ", "hansuu": n_akahai})
+
+    n_uradora = 0
+
+    for hai in uradora or []:
+        hai = Yama.dora(hai)
+        regexp = re.compile(hai[1])
+
+        for suit_string in suit_string_list:
+            if suit_string[0] != suit_string[0]:
+                continue
+
+            suit_string = suit_string.replace("0", "5")
+            numbers = regexp.findall(suit_string)
+
+            if numbers:
+                n_uradora += len(numbers)
+
+    if n_uradora:
+        post_yaku.append({"name": "裏ドラ", "hansuu": n_uradora})
+
+    return post_yaku
