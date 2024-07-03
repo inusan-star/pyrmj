6f5ffae971a65959a73f03b21c0a87339467b0df
yokoyamaharuki <haruki.yokoyama@rooter.co.jp>
2024-07-03 15:07:41 +0900

    gameクラスの雛形を追加

--- src/pyrmj/__init__.py
+++ src/pyrmj/__init__.py
@@ -1,3 +1,4 @@
+from .game import *
 from .hoora import *
 from .kawa import *
 from .rule import *
---
+++ src/pyrmj/game.py
@@ -0,0 +1,95 @@
+import random
+import datetime
+from .rule import rule
+
+
+class Game:
+    """
+    ゲーム進行を管理するクラス
+    """
+
+    KAIKYOKU = "kaikyoku"
+
+    def __init__(self, players, callback=None, rule_json=None, title=None):
+        self.players_ = players
+        self.callback_ = callback or (lambda: None)
+        self.rule_ = rule_json or rule()
+
+        self.model_ = {
+            "title": title or "pyrmj - " + datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S"),
+            "player": ["自家", "下家", "対面", "上家"],
+            "chiicha": 0,
+            "bakaze": 0,
+            "kyokusuu": 0,
+            "tsumibou": 0,
+            "riichibou": 0,
+            "tokuten": [self.rule_["配給原点"]] * 4,
+            "yama": None,
+            "tehai": [],
+            "kawa": [],
+            "player_id": [0, 1, 2, 3],
+        }
+
+        self.max_kyokusuu_ = None
+        self.haifu_ = {}
+        self.status_ = None
+        self.reply_ = []
+
+    def step(self, actions):
+        """
+        対局を進める
+        """
+        self.reply_ = [None] * 4
+
+        for player_id, action in actions.items():
+            self.reply_[player_id] = action or {}
+
+        if self.status_ == self.KAIKYOKU:
+            self.reply_kaikyoku()
+
+    def kaikyoku(self, chiicha=None):
+        """
+        開局する
+        """
+        random.seed(1704034800)  # シード値を設定（Time stamp of 1/1/2024）
+        self.model_["chiicha"] = chiicha if chiicha else random.randint(0, 3)
+        self.max_kyokusuu_ = 0 if self.rule_["場数"] == 0 else self.rule_["場数"] * 4 - 1
+
+        self.haifu_ = {
+            "title": self.model_["title"],
+            "player": self.model_["player"],
+            "chiicha": self.model_["chiicha"],
+            "log": [],
+            "tokuten": self.model_["tokuten"][:],
+            "point": [],
+            "rank": [],
+        }
+
+        observation = []
+
+        for player_id in range(4):
+            observation.append(
+                {
+                    "kaikyoku": {
+                        "id": player_id,
+                        "rule": self.rule_,
+                        "title": self.haifu_["title"],
+                        "player": self.haifu_["player"],
+                        "chiicha": self.haifu_["chiicha"],
+                    }
+                }
+            )
+
+        self.status_ = self.KAIKYOKU
+        return observation
+
+    def reply_kaikyoku(self):
+        """
+        開局の応答に対する処理
+        """
+        self.haipai()
+
+    def haipai(self):
+        """
+        配牌する
+        """
